fd0f9e645dddca8275de96fbd6613f58f8ffe4ec
George Yu <george@think.digital>
2024-01-10 13:50:00 +1030

    Completed project 2 keep it going

--- Master DOM Manipulation P2/project-1.js
+++ Master DOM Manipulation P2/project-1.js
@@ -32,18 +32,6 @@ const quotes = [{
     person: `Lao Tzu`
 }, ];
 
-// let btn = document.querySelector('#new-quote');
-// let quote = document.querySelector('.quote');
-// let person = document.querySelector('.person');
-
-// btn.addEventListener('click', function(){
-
-//     let random = Math.floor(Math.random() * quotes.length);
-
-//     quote.innerText = quotes[random].quote;
-//     person.innerText = quotes[random].person;
-
-// })
 
 let btn = document.querySelector('#new-quote');
 let author = document.querySelector('.person');
--- Master DOM Manipulation P2/project-2.js
+++ Master DOM Manipulation P2/project-2.js
@@ -1,24 +1,46 @@
 // Variables
 
-let openBtn = document.getElementById('open-btn');
-let modalContainer = document.getElementById('modal-container');
-let closeBtn = document.getElementById('close-btn');
+// let openBtn = document.getElementById('open-btn');
+// let modalContainer = document.getElementById('modal-container');
+// let closeBtn = document.getElementById('close-btn');
 
 // Event Listeners 
 
-openBtn.addEventListener('click', function(){
+// openBtn.addEventListener('click', function(){
 
-    modalContainer.style.display = 'block';
-});
+//     modalContainer.style.display = 'block';
+// });
 
-closeBtn.addEventListener('click', function() {
+// closeBtn.addEventListener('click', function() {
 
-    modalContainer.style.display = 'none';
-});
+//     modalContainer.style.display = 'none';
+// });
 
-window.addEventListener('click', function(e) {
+// window.addEventListener('click', function(e) {
 
-    if (e.target === modalContainer) {
-        modalContainer.style.display = 'none';
+//     if (e.target === modalContainer) {
+//         modalContainer.style.display = 'none';
+//     }
+// });
+
+
+
+
+const openBtn = document.querySelector('#open-btn');
+const modal = document.querySelector('#modal-container');
+const closeBtn = document.querySelector('#close-btn');
+
+openBtn.addEventListener('click', function(e){
+    modal.style.display = 'block';
+})
+
+closeBtn.addEventListener('click', function(e){
+    modal.style.display = 'none';
+})
+
+window.addEventListener('click', function(e){
+    console.log(e.target)
+    if (e.target === modal){
+        modal.style.display = 'none';
     }
-});
+})
