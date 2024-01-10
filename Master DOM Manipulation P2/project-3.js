565a9fbc586ed16d8c2e6204de284cfff36fba8b
George Yu <george@think.digital>
2024-01-10 14:29:46 +1030

    Completed project 3, yes sir

--- Master DOM Manipulation P2/project-3.js
+++ Master DOM Manipulation P2/project-3.js
@@ -4,8 +4,17 @@
 
 // for( i = 0; i < accordion.length; i++) {
 
-    accordion[i].addEventListener('click', function () {
+//     accordion[i].addEventListener('click', function () {
+//         this.classList.toggle('active');
+//     })
+
+// };
+
+
+
+let qns = document.querySelectorAll('.content-container');
+for (let i=0; i<qns.length; i++){
+    qns[i].addEventListener('click', function(e){
         this.classList.toggle('active');
     })
-
-};
+}
